00c9db21624e7da5db3dd8fc02d9acd034e6ecd7
Zena <zena.vilenia@gmail.com>
2018-03-06 15:59:12 +0700

    Tugas Binary Search Zena Vilenia

--- binary.js
+++ binary.js
@@ -5,40 +5,63 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
-  // Selection sort
+  // Insertion sort
   for(let i = 0; i < arr.length; i++) {
-    let tmp = i;
-    for(let j = i+1; j < arr.length; j++) {
-      if(arr[j] < arr[tmp]) {
+    var tmp = i;
+    for(let j = i-1; j >= 0; j--) {
+      if(arr[j] > arr[tmp]) {
+        var tmps = arr[tmp];
+        arr[tmp] = arr[j];
+        arr[j] = tmps;
         tmp = j;
       }
     }
-    //[arr[tmp], arr[i]] = [arr[i], arr[tmp]];
-    let tmp1 = arr[i];
-    arr[i] = arr[tmp];
-    arr[tmp] = tmp1;
   }
-
   return arr;
 }
 
+// function binarySearch (search, array) {
+//   // Your searching code
+//   if(array.length === 1) {
+//     if(array[0] !== search) {
+//       return -1;
+//     }
+//     return 0;
+//   }
+//
+//   let mid = Math.floor(array.length/2);
+//   if(search === array[mid]) {
+//     return mid;
+//   } else if(search > array[mid]) {
+//     array = array.slice(mid, array.length);
+//     return mid + binarySearch(search, array);
+//   } else {
+//     array = array.slice(0, mid);
+//     return binarySearch(search, array);
+//   }
+// }
+
 function binarySearch (search, array) {
   // Your searching code
-  console.log(array);
-  if(array.length === 1) {
-    return -1;
+  let start = 0;
+  let finish = array.length-1;
+  let mid = Math.floor(array.length/2)-1;
+
+  while((mid != start) && (mid != finish)) {
+    if(search === array[mid]) {
+      return mid;
+    } else if(array[mid] > search) {
+      start = 0;
+      finish = mid-1;
+      mid = Math.floor(start+finish/2);
+    } else {
+      start = mid+1;
+      finish = array.length-1;
+      mid = Math.floor(start+finish/2);
+    }
   }
 
-  let mid = Math.floor(array.length/2);
-  if(search === array[mid]) {
-    return mid;
-  } else if(search > array[mid]) {
-    array = array.slice(mid, array.length);
-    return binarySearch(search, array);
-  } else {
-    array = array.slice(0, mid);
-    return binarySearch(search, array);
-  }
+  return (array[mid] === search) ? mid : -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap);
@@ -50,6 +73,7 @@ console.log(binarySearch(8, arrayGenapSorted));
 console.log(binarySearch(10, arrayGenapSorted));
 console.log(binarySearch(33, arrayGenapSorted));
 
+console.log(arrayGanjilSorted);
 console.log(binarySearch(53, arrayGanjilSorted));
 console.log(binarySearch(3, arrayGanjilSorted));
 console.log(binarySearch(2, arrayGanjilSorted));
