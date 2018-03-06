22759517b889df41f325bd7b4f1d29b30a3865c2
agungcaproex <agung.caproex@gmail.com>
2018-03-06 16:28:59 +0700

    sixth commit day 2

--- binary.js
+++ binary.js
@@ -5,26 +5,57 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  for(let i=0; i<arr.length; i++){
+    let sorted = arr[i]
+    let j = i
+
+    while(j>0 && arr[j-1] > sorted){
+      arr[j] = arr[j-1]
+      j--
+      
+    }
+    // console.log(arr[j])
+    // console.log('=',sorted)
+    arr[j] =  sorted
+  }
+  console.log(arr)
   return arr
 }
 
 function binarySearch (search, array) {
   // Your searching code
-  return 0;
+  let min = 0
+  let max = array.length - 1
+  
+  while(min <= max){
+    let mid = Math.floor((max + min)/2)
+    
+    if(array[mid] == search){
+      return mid
+    }
+    else if(array[mid] > search){
+      max = mid - 1
+    }
+    else{
+      min = mid + 1
+    }
+  }
+  return -1
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
-// Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+console.log('====BINARY SEARCH====')
+// // Driver code
+console.log('index ke : ',binarySearch(8, arrayGenapSorted));
+console.log('index ke : ',binarySearch(10, arrayGenapSorted));
+console.log(binarySearch(33, arrayGenapSorted));
 
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
+console.log('index ke : ',binarySearch(53, arrayGanjilSorted));
+console.log('index ke : ',binarySearch(3, arrayGanjilSorted));
+console.log(binarySearch(2, arrayGanjilSorted));
 
 module.exports = {
-  binary_search
+  binarySearch
 }
