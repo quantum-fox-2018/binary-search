fdd4672df924f37f34418a24734b2780d628546a
barestu <fheartnet@gmail.com>
2018-03-06 16:14:11 +0700

    Soal Binary Search

--- binary.js
+++ binary.js
@@ -5,12 +5,47 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
-  return arr
+  for (let i = 0; i < arr.length; i++) {
+    var indexMin = i;
+
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[j] < arr[indexMin]) {
+        indexMin = j;
+      }
+    }
+
+    if (indexMin !== i) {
+      var temp = arr[i];
+      arr[i] = arr[indexMin];
+      arr[indexMin] = temp;
+
+    }
+  }
+
+  return arr;
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binary_search (search, array) {
+  // Your searching cod
+  var left = 0;
+  var right = array.length - 1;
+
+  console.log(array);
+
+  while (left <= right) {
+    debugger;
+    var middle = Math.floor((left + right) / 2);
+    if (search === array[middle]) {
+      return middle;
+    } else if (search < array[middle]) {
+      right = middle-1;
+    } else if (search > array[middle]) {
+      left = middle+1;
+    }
+
+  }
+
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
@@ -20,7 +55,7 @@ var arrayGanjilSorted = ownSort(testArrayGanjil)
 console.log(binary_search(8, arrayGenapSorted))
 console.log(binary_search(10, arrayGenapSorted))
 console.log(binary_search(33, arrayGenapSorted))
-
+console.log('=============================================');
 console.log(binary_search(53, arrayGanjilSorted))
 console.log(binary_search(3, arrayGanjilSorted))
 console.log(binary_search(2, arrayGanjilSorted))
