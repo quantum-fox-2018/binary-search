d9dfaaee4fb984bb483b3c4f59d26ca0537c9a91
Fitrul Islam <fitrul.islam@gmail.com>
2018-03-06 13:05:32 +0700

    done

--- binary.js
+++ binary.js
@@ -4,13 +4,35 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
-  return arr
+  for(let i=1; i<arr.length; i++){
+    for(let j=0; j<=i-1; j++){
+      let front = arr[j];
+      let back = arr[i];
+      if(arr[i]<arr[j]){
+        arr[i] = front;
+        arr[j] = back;
+      }
+    }
+  }
+  return arr;
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binary_search (search, array) {
+  let bot = 0;
+  let top = array.length-1;
+  while(bot<=top){
+    let mid = Math.floor((bot+top)/2);
+    if(search == array[mid]){
+      return mid;
+    } else {
+      if(search > array[mid]){
+        bot = mid+1;
+      } else {
+        top = mid-1;
+      }
+    }
+  }
+  return -1
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
