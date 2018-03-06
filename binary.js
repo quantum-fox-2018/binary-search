97371af67c6041f12c650e8a3ca44bbac54d5cd9
Taufik Hidayat <ovick_kd@yahoo.com>
2018-03-06 15:09:28 +0700

    Selection sort

--- binary.js
+++ binary.js
@@ -5,26 +5,27 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
-  return arr
-}
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+    for (let i = 0; i < arr.length; i++) {
+
+      for (let j = i; j < arr.length; j++) {
+
+        let temp = arr[i]
+
+        if (arr[i] > arr[j]) {
+
+          arr[i] = arr[j]
+          arr[j] = temp
+
+        }
+
+      }
+
+    }
+
+  console.log(arr);
+
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
-
-// Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
-
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
-
-module.exports = {
-  binary_search
-}
