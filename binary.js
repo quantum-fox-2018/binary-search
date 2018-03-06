77cdab708a5d858e5867c087ce8034105c9d8e64
Zena <zena.vilenia@gmail.com>
2018-03-06 14:35:38 +0700

    Tugas Binary Search Zena Vilenia

--- binary.js
+++ binary.js
@@ -5,26 +5,55 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
-  return arr
+  // Selection sort
+  for(let i = 0; i < arr.length; i++) {
+    let tmp = i;
+    for(let j = i+1; j < arr.length; j++) {
+      if(arr[j] < arr[tmp]) {
+        tmp = j;
+      }
+    }
+    //[arr[tmp], arr[i]] = [arr[i], arr[tmp]];
+    let tmp1 = arr[i];
+    arr[i] = arr[tmp];
+    arr[tmp] = tmp1;
+  }
+
+  return arr;
 }
 
 function binarySearch (search, array) {
   // Your searching code
-  return 0;
+  console.log(array);
+  if(array.length === 1) {
+    return -1;
+  }
+
+  let mid = Math.floor(array.length/2);
+  if(search === array[mid]) {
+    return mid;
+  } else if(search > array[mid]) {
+    array = array.slice(mid, array.length);
+    return binarySearch(search, array);
+  } else {
+    array = array.slice(0, mid);
+    return binarySearch(search, array);
+  }
 }
 
-var arrayGenapSorted = ownSort(testArrayGenap)
-var arrayGanjilSorted = ownSort(testArrayGanjil)
+var arrayGenapSorted = ownSort(testArrayGenap);
+var arrayGanjilSorted = ownSort(testArrayGanjil);
 
+console.log(arrayGenapSorted);
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+console.log(binarySearch(8, arrayGenapSorted));
+console.log(binarySearch(10, arrayGenapSorted));
+console.log(binarySearch(33, arrayGenapSorted));
 
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
+console.log(binarySearch(53, arrayGanjilSorted));
+console.log(binarySearch(3, arrayGanjilSorted));
+console.log(binarySearch(2, arrayGanjilSorted));
 
-module.exports = {
-  binary_search
-}
+// module.exports = {
+//   binary_search
+// }
