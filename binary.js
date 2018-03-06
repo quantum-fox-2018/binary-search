7992e3140adf71823558dc2902929d4c822f77a8
JoviAndrew <jovian_andrew@hotmail.com>
2018-03-06 15:59:11 +0700

    submit binary search

--- binary.js
+++ binary.js
@@ -5,26 +5,82 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
-  return arr
+  for(let counter = 0; counter<arr.length; counter++){
+    for(let counter2 = counter; counter2<arr.length; counter2++){
+      if(arr[counter] > arr[counter2]){
+        let temp = arr[counter];
+        arr[counter] = arr[counter2];
+        arr[counter2] = temp;
+      }
+    }
+  }
+  return arr;
 }
 
 function binarySearch (search, array) {
   // Your searching code
-  return 0;
+  //RECURSIVE not finished!
+  // if(array.length ===  1 && array[0] !== search) return -1
+  // else{
+  //   let halfArrLength = Math.floor((array.length ) /2);
+
+  //   if(array[halfArrLength] === search) return halfArrLength;
+  //   else{
+  //     let array1 = [];
+  //     let array2 = [];
+
+  //     for(let counter = 0; counter<array.length; counter++){
+  //       if(counter<halfArrLength){
+  //         array1.push(array[counter]);
+  //       } 
+  //       else {
+  //         array2.push(array[counter]);
+  //       }
+  //     }
+
+  //     console.log('This is Arr1: '+array1);
+  //     console.log('This is Arr2: '+array2);
+
+  //     if(array[halfArrLength] > search) return binarySearch(search, array1);
+  //     else return halfArrLength + binarySearch(search, array2);
+  //   }
+  // }
+
+  let low = 0;
+  let mid = Math.floor(array.length /2) -1;
+  let high = array.length-1;
+  while(low !== mid && low !== high){
+    if(array[mid] === search) {
+      return mid;
+    }
+    else if(array[mid] > search){
+      high = mid;
+      mid = Math.floor((low + high) /2);
+    }else if(array[mid] < search){
+      low = mid;
+      mid = Math.floor((low + high) /2);
+    }
+  }
+  if(array[mid] === search) {
+    return mid;
+  }else{
+    return -1
+  }
 }
+
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+console.log(binarySearch(8, arrayGenapSorted))    //0
+console.log(binarySearch(10, arrayGenapSorted))   //1
+console.log(binarySearch(33, arrayGenapSorted))   //-1
 
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
+console.log(binarySearch(53, arrayGanjilSorted))  //3
+console.log(binarySearch(3, arrayGanjilSorted))   //0
+console.log(binarySearch(2, arrayGanjilSorted))   //-1
 
 module.exports = {
-  binary_search
+  binarySearch
 }
